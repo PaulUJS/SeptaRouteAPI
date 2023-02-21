e88d5cd740f5c8cedf64f0bd00216855f0e6f524
Paul Ulerick <p.ulerick17@gmail.com>
2023-02-21 16:34:36 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/json"));
+client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+await ProcessApiAsync(client);
+
+static async Task ProcessApiAsync(HttpClient client)
+{
+    var json =
+        await client.GetStringAsync("https://www3.septa.org/api/TransitView/index.php/47");
+    Console.Write(json);
+
+    return;
+}
+
